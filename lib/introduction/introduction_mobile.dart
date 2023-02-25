21c0236b52bd79cc92afe9cefd1323c390275bb2
AmarjitM13 <mallickamarjit@gmail.com>
2023-02-25 23:56:15 +0530

    mobile about section updated

--- lib/introduction/introduction_mobile.dart
+++ lib/introduction/introduction_mobile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/theme/theme_switcher.dart';
 import 'package:portfolio/utilities/constants.dart';
 
 class IntroductionMobile extends StatefulWidget {
@@ -11,205 +12,50 @@ class _IntroductionMobileState extends State<IntroductionMobile> {
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height * 2,
-      color: Color(0xFF25262A),
+      height: MediaQuery.of(context).size.height,
+      padding: EdgeInsets.symmetric(vertical: 70, horizontal: 50),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width / 2,
+          Text(
+            'a little about me ',
+            style: TextStyle(
+              color: ThemeSwitcher.of(context).isDarkModeOn ? Colors.white : Colors.black,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'RobotoMono',
+            ),
+          ),
+          Expanded(
             child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 20),
-                  Text(
-                    'Introduce',
-                    style: TextStyle(
-                        color: Colors.white.withOpacity(0.5),
-                        fontFamily: 'RobotoMono',
-                        fontSize: 10),
+              child: RichText(
+                text: TextSpan(
+                  text: "I've been sailing the app development seas more than 2 years. "
+                      "I've been creating everything from regular apps and dashboards,"
+                      " all the way to complex mobile applications."
+                      "\n\nThe main characteristic of my work one could give for all these years has been simple:\n\n",
+                  style: TextStyle(
+                    color: ThemeSwitcher.of(context).isDarkModeOn ? Colors.white : Colors.black,
+                    fontFamily: 'RobotoMono',
+                    fontSize: 20,
                   ),
-                  Text(
-                    "Hello!\nI'm Amarjit\nMallick",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'RobotoMono',
-                        fontSize: 30),
-                  ),
-                  Text(
-                    'Every great design begin with an\neven better story',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontFamily: 'RobotoMono',
-                        fontSize: 20,
-                        fontStyle: FontStyle.italic),
-                  ),
-                  Text(
-                    'I design and code beautifully simple things\nand I love what I do.',
-                    style: TextStyle(
-                        color: Colors.white.withOpacity(0.5),
-                        fontFamily: 'RobotoMono',
-                        fontSize: 10),
-                  ),
-                  SizedBox(height: 60),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height / 3 - 40,
-            width: MediaQuery.of(context).size.width - 40,
-            decoration: BoxDecoration(
-              color: kPrimaryColor,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.35),
-                  blurRadius: 5.0, // soften the shadow
-                  spreadRadius: 3.0, //extend the shadow
+                  children: [
+                    TextSpan(
+                      text: "Get things done. Fast!!!!!",
+                      style: TextStyle(
+                          color: ThemeSwitcher.of(context).isDarkModeOn ? Color(0xFF00D1C7) : Color(0xFF646AFF),
+                          fontFamily: 'RobotoMono',
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                        text:
+                            "\n\nI write clean and modern Dart code with a rigid code-style, and I do it really fast. "),
+                  ],
                 ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Design',
-                        style: TextStyle(
-                            color: Colors.green,
-                            fontFamily: 'RobotoMono',
-                            fontSize: 25),
-                      ),
-                      Icon(
-                        Icons.settings_ethernet,
-                        color: Colors.green,
-                      )
-                    ],
-                  ),
-                  Text(
-                    'data',
-                    style: TextStyle(
-                        color: Colors.white.withOpacity(0.5),
-                        fontFamily: 'RobotoMono',
-                        fontSize: 10),
-                  ),
-                  Text(
-                    'data',
-                    style: TextStyle(color: Colors.white, fontSize: 11),
-                  ),
-                ],
               ),
             ),
           ),
-          Container(
-            height: MediaQuery.of(context).size.height / 3 - 40,
-            width: MediaQuery.of(context).size.width - 40,
-            decoration: BoxDecoration(
-              color: kPrimaryColor,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.35),
-                  blurRadius: 5.0, // soften the shadow
-                  spreadRadius: 3.0, //extend the shadow
-                ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Design',
-                        style: TextStyle(
-                            color: Colors.green,
-                            fontFamily: 'RobotoMono',
-                            fontSize: 25),
-                      ),
-                      Icon(
-                        Icons.settings_ethernet,
-                        color: Colors.green,
-                      )
-                    ],
-                  ),
-                  Text(
-                    'data',
-                    style: TextStyle(
-                        color: Colors.white.withOpacity(0.5),
-                        fontFamily: 'RobotoMono',
-                        fontSize: 10),
-                  ),
-                  Text(
-                    'data',
-                    style: TextStyle(color: Colors.white, fontSize: 11),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height / 3 - 40,
-            width: MediaQuery.of(context).size.width - 40,
-            decoration: BoxDecoration(
-              color: kPrimaryColor,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.35),
-                  blurRadius: 5.0, // soften the shadow
-                  spreadRadius: 3.0, //extend the shadow
-                ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Design',
-                        style: TextStyle(
-                            color: Colors.green,
-                            fontFamily: 'RobotoMono',
-                            fontSize: 25),
-                      ),
-                      Icon(
-                        Icons.settings_ethernet,
-                        color: Colors.green,
-                      )
-                    ],
-                  ),
-                  Text(
-                    'data',
-                    style: TextStyle(
-                        color: Colors.white.withOpacity(0.5),
-                        fontFamily: 'RobotoMono',
-                        fontSize: 10),
-                  ),
-                  Text(
-                    'data',
-                    style: TextStyle(color: Colors.white, fontSize: 11),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
         ],
       ),
     );
